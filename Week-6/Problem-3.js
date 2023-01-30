ac09505003519a80d9b8018ed0896020a5636ba6
Dipesh0511 <116885406+Dipesh0511@users.noreply.github.com>
2023-01-30 14:13:28 +0530

    Update Problem-3.js

--- Week-6/Problem-3.js
+++ Week-6/Problem-3.js
@@ -1,3 +1,13 @@
-let num = [0, 1, 1, 1, 2, 2, 2, 0, 0, ]
+let num = [0, 1, 1, 1, 2, 2, 2, 0, 0],
+    temp = 0;
 console.log(num);
-console.log(num.sort());
+for (let i = 0; i < num.length; i++) {
+    for (let j = i; j < num.length; j++) {
+        if (num[i] > num[j]) {
+            temp = num[i];
+            num[i] = num[j];
+            num[j] = temp;
+        }
+    }
+}
+console.log(num)
